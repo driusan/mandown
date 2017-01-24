f49bc13fe1d9eeedc4e01a12750c2ed5c75fb35d
Dave MacFarlane <driusan@gmail.com>
2017-01-24 18:36:17 -0500

    Added bold and underline support to troff output
    This isn't strictly in line with the markdown spec, which treats "*" and "_" as
    interchangable, and doesn't actually say anything about "bold" or "underline",
    but we use a regex to replace *word* with bold, _word_ with underline, and
    __words words words__ with a continuous underline (underline, including the spaces
    between words, while underline only underlines the printable characters in troff.)

--- roff.go
+++ roff.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 )
+
+var boldRe *regexp.Regexp = regexp.MustCompile(`\*(.+?)\*([ \t]*)`)
+var culRe *regexp.Regexp = regexp.MustCompile(`__(.+?)__([ \t]*)`)
+var ulRe *regexp.Regexp = regexp.MustCompile(`_(.+?)_([ \t]*)`)
 
 func (mp ManPage) TroffString() string {
 	r := fmt.Sprintf(".TH %s %d\n", strings.ToUpper(mp.Name), mp.Section)
@@ -18,7 +23,17 @@ func (mp ManPage) TroffString() string {
 		} else {
 			r += fmt.Sprintf(".SH %s\n", strings.ToUpper(s.Name))
 		}
-		r += strings.Replace(s.Content, "\n\n", "\n.PP\n", -1) + "\n"
+
+		// Do a couple transformations
+		// Paragraphs
+		content := strings.Replace(s.Content, "\n\n", "\n.PP\n", -1) + "\n"
+		// *Bold*
+		content = boldRe.ReplaceAllString(content, "\n.B $1\n")
+		// __continuous underline__
+		content = culRe.ReplaceAllString(content, "\n.cu\n$1\n")
+		// _underline_
+		content = ulRe.ReplaceAllString(content, "\n.ul\n$1\n")
+		r += fmt.Sprintf(content)
 
 	}
 	return r
